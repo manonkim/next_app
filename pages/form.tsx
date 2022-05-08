62acfe9eccb816b17341bbd3429f86532a020867
WY K <ablack1029@gmail.com>
2022-05-08 21:32:56 +0900

    Add : register로 수정

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,58 +1,21 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Forms() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [formErrors, setFormErrors] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const onUsernameChanege = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setUsername(value);
-  };
-  const onEmailChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setEmail(value);
-  };
-  const onPasswordChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setPassword(value);
-  };
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (username === '' || email === '' || password === '') {
-      setFormErrors('All fields are required');
-    }
-    if (!email.includes('@')) {
-      setEmailError('~~');
-    }
-  };
+  const { register, watch } = useForm();
+
   return (
-    <form onSubmit={onSubmit}>
+    <form>
       <input
-        value={username}
-        onChange={onUsernameChanege}
+        {...register('username')}
         type="text"
         placeholder="Username"
         required
         minLength={5}
       />
+      <input {...register('email')} type="email" placeholder="Email" required />
       <input
-        value={email}
-        onChange={onEmailChange}
-        type="email"
-        placeholder="Email"
-        required
-      />
-      <input
-        value={password}
-        onChange={onPasswordChange}
+        {...register('password')}
         type="password"
         placeholder="Password"
         required
