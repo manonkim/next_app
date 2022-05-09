184ec30bb06a9eae09cb261ab8ffe8330bd66845
WY K <ablack1029@gmail.com>
2022-05-09 23:54:12 +0900

    Add : react hooks form 사용

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,24 +1,68 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldError, FieldErrors, useForm } from 'react-hook-form';
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setError,
+    reset,
+    resetField,
+  } = useForm<LoginForm>({
+    mode: 'onChange',
+  });
+  const onValid = (data: LoginForm) => {
+    console.log('hi');
+    reset();
+    resetField('email');
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register('username')}
+        {...register('username', {
+          required: 'Username is required',
+          minLength: {
+            message: 'Username은 5자 이상으로 입력해야 합니다.',
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
-        minLength={5}
       />
-      <input {...register('email')} type="email" placeholder="Email" required />
+      {errors.username?.message}
       <input
-        {...register('password')}
+        {...register('email', {
+          required: 'Email is required',
+          validate: {
+            notGmail: (value) =>
+              !value.includes('@gmail.com')
+                ? ''
+                : 'gmail은 사용하실 수 없습니다.',
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${Boolean(errors.email?.message) ? 'border-red-600' : ''}`}
+      />
+      {errors.email?.message}
+      <input
+        {...register('password', {
+          required: 'password is required',
+        })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
