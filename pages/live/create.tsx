82b92a78f89c8b184d3863ff2d9a8e1c18700a4e
WY <ablack1029@gmail.com>
2022-11-14 01:03:52 +0900

    Feature : form submission

--- pages/live/create.tsx
+++ pages/live/create.tsx
@@ -6,19 +6,12 @@ import TextArea from '../../components/textarea';
 
 const Create: NextPage = () => {
   return (
-    <Layout canGoBack title="Go Live">
-      <form className=" space-y-4 py-10 px-4">
-        <Input required label="Name" name="name" type="text" />
-        <Input
-          required
-          label="Price"
-          placeholder="0.00"
-          name="price"
-          type="text"
-          kind="price"
-        />
-        <TextArea name="description" label="Description" />
-        <Button text="Go live" />
+    <Layout canGoBack title='Go Live'>
+      <form className=' space-y-4 py-10 px-4'>
+        <Input required label='Name' name='name' type='text' />
+        <Input required label='Price' name='price' type='text' kind='price' />
+        <TextArea name='description' label='Description' />
+        <Button text='Go live' />
       </form>
     </Layout>
   );
